170b824869fe09ca34a0c2395fec7bea33b19ddf
Alex Pu <alexander.r.pu@gmail.com>
2023-01-31 18:37:31 -0800

    Added basic testing project

---
+++ HomeCareServiceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HomeCareServiceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeCareServiceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23e80656-fe08-4c2d-ba6f-0e068c02850a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HomeCareServiceTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace HomeCareServiceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string url = "http://localhost:49198/Clients/Create";
+            string url = "http://localhost:50277";
+            //string url = "http://localhost:49198";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.FindElement(By.LinkText("Client")).Click();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.FindElement(By.LinkText("Create New")).Click();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.FindElement(By.Id("Name")).SendKeys("Marge Simpson");
+            //driver.FindElement(By.Id("Address")).SendKeys("Springfield");
+            //driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+    }
+}
