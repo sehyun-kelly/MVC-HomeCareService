c47d7976c5b6049c495624dc09b1d6570cea9241
Alex Pu <alexander.r.pu@gmail.com>
2023-02-02 15:46:03 -0800

    added more tests and added id to checkbox in add customer page

--- HomeCareServiceTests/UnitTest1.cs
+++ HomeCareServiceTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 
 namespace HomeCareServiceTests
@@ -10,7 +11,7 @@ namespace HomeCareServiceTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1_AddService()
         {
             //string url = "http://localhost:49198/Clients/Create";
             string url = "http://localhost:50277";
@@ -18,14 +19,54 @@ namespace HomeCareServiceTests
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            //driver.FindElement(By.LinkText("Client")).Click();
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            //driver.FindElement(By.LinkText("Create New")).Click();
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            //driver.FindElement(By.Id("Name")).SendKeys("Marge Simpson");
-            //driver.FindElement(By.Id("Address")).SendKeys("Springfield");
-            //driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create a new Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Home");
+            driver.FindElement(By.Id("Price")).SendKeys("1000");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+        public void Test2_AddEmployee()
+        {
+            string url = "http://localhost:50277";
+            //string url = "http://localhost:49198";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Employee")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Add a New Employee")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Worker");
+            driver.FindElement(By.Id("Address")).SendKeys("BCIT");
+            driver.FindElement(By.Id("Salary")).SendKeys("10000");
+            SelectElement dropDown = new SelectElement(driver.FindElement(By.XPath("//*[@id='services_ID']")));
+            dropDown.SelectByText("Home");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+        public void Test3_AddCustomer()
+        {
+            string url = "http://localhost:50277";
+            //string url = "http://localhost:49198";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Customer")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Add a new Customer")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Client");
+            driver.FindElement(By.Id("Address")).SendKeys("Vancouver");
+            driver.FindElement(By.Id("Home")).Click();
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
     }
 }
