3df97dab8cb31cf2d60dd6389011b6a48134f0db
Melanie Ta <nngoc2701@gmail.com>
2023-02-03 01:32:51 -0800

    Add close command after each unit tests

--- HomeCareServiceTests/UnitTest1.cs
+++ HomeCareServiceTests/UnitTest1.cs
@@ -13,9 +13,7 @@ namespace HomeCareServiceTests
         [TestMethod]
         public void Test1_AddService()
         {
-            //string url = "http://localhost:49198/Clients/Create";
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -27,13 +25,12 @@ namespace HomeCareServiceTests
             driver.FindElement(By.Id("Name")).SendKeys("Home");
             driver.FindElement(By.Id("Price")).SendKeys("1000");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         public void Test1_AddService2()
         {
-            //string url = "http://localhost:49198/Clients/Create";
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -45,13 +42,13 @@ namespace HomeCareServiceTests
             driver.FindElement(By.Id("Name")).SendKeys("Work");
             driver.FindElement(By.Id("Price")).SendKeys("1000");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test2_AddEmployee()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -66,13 +63,13 @@ namespace HomeCareServiceTests
             SelectElement dropDown = new SelectElement(driver.FindElement(By.XPath("//*[@id='services_ID']")));
             dropDown.SelectByText("Home");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test2_AddEmployee2()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -87,13 +84,13 @@ namespace HomeCareServiceTests
             SelectElement dropDown = new SelectElement(driver.FindElement(By.XPath("//*[@id='services_ID']")));
             dropDown.SelectByText("Home");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test3_AddCustomer()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -106,13 +103,13 @@ namespace HomeCareServiceTests
             driver.FindElement(By.Id("Address")).SendKeys("Vancouver");
             driver.FindElement(By.Id("Home")).Click();
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test3_AddCustomer2()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -125,12 +122,12 @@ namespace HomeCareServiceTests
             driver.FindElement(By.Id("Address")).SendKeys("Vancouver");
             driver.FindElement(By.Id("Home")).Click();
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test4_EditService() {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -144,13 +141,13 @@ namespace HomeCareServiceTests
             driver.FindElement(By.XPath("//input[@id='Price']")).Clear();
             driver.FindElement(By.XPath("//input[@id='Price']")).SendKeys("2000");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test5_EditEmployee()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -166,13 +163,13 @@ namespace HomeCareServiceTests
             driver.FindElement(By.XPath("//input[@id='Salary']")).Clear();
             driver.FindElement(By.XPath("//input[@id='Salary']")).SendKeys("2000");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test6_EditCustomer()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -187,28 +184,13 @@ namespace HomeCareServiceTests
             driver.FindElement(By.XPath("//input[@id='Address']")).SendKeys("Burnaby");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
-        public void Test7_DeleteService()
+        public void Test7_DeleteEmployee()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
-            ChromeDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.FindElement(By.LinkText("Service")).Click();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.FindElement(By.LinkText("Delete")).Click();
-            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
-        }
-
-        [TestMethod]
-        public void Test8_DeleteEmployee()
-        {
-            string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -217,13 +199,28 @@ namespace HomeCareServiceTests
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.FindElement(By.LinkText("Delete")).Click();
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void Test8_DeleteService()
+        {
+            string url = "http://localhost:50277";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Delete")).Click();
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
 
         [TestMethod]
         public void Test9_DeleteCustomer()
         {
             string url = "http://localhost:50277";
-            //string url = "http://localhost:49198";
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
@@ -232,6 +229,7 @@ namespace HomeCareServiceTests
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.FindElement(By.LinkText("Delete")).Click();
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Close();
         }
     }
 }
